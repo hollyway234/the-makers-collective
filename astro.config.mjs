0d8fe21f37a4e9fd3106a382dd1fde8d1c8eb3b5
hollyway234 <hollyway@outlook.co.nz>
2026-09-07 11:51:45 +1200

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// Once your domain is registered, set it here — it helps with SEO and social sharing previews.
-export default defineConfig({
-  site: 'https://waihekemakers.co.nz',
-});
